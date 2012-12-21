b133df56253481b44b2ca1e7c5a6b2ace40be7a2
daclouds <daclouds@gmail.com>
2012-12-22 01:18:52 +0900

    simple http static server

--- src/main/scala/com/npcode/Scalahttpd.scala
+++ src/main/scala/com/npcode/Scalahttpd.scala
@@ -8,6 +8,7 @@ import org.apache.tika.Tika
 import java.io.FileInputStream
 import scala.util.matching.Regex
 import akka.actor.IO.ReadHandle
+import java.nio.charset.Charset
 
 trait HTTP {
   def ??? : Nothing = throw new Error("an implementation is missing")
@@ -19,19 +20,19 @@ trait HTTP {
   def mimeType(file: File) = new Tika().detect(file)
 
   // Get request line from request
-  def requestLine(bytes: ByteString): String = ???
+  def requestLine(bytes: ByteString): String = bytes.decodeString("UTF-8")
 
   // Return "404 Not Found" response.
-  def notFound: ByteString = ???
+  def notFound: ByteString = ByteString("HTTP/1.1 404 Not Found")
 
   // Return "405 Method Not Allowed" response.
-  def methodNotAllowed: ByteString = ???
+  def methodNotAllowed: ByteString = ByteString("HTTP/1.1 405 Method Not Allowed")
 
   // Return "400 Bad Request" response.
-  def badRequest: ByteString = ???
+  def badRequest: ByteString = ByteString("HTTP/1.1 400 Bad Request")
 
   // Return 200 OK response with the given file.
-  def ok(file: File): ByteString = ???
+  def ok(file: File): ByteString = readFile(file)
 
   def serve(rHandle: ReadHandle, request: ByteString) = {
     rHandle.asSocket.write(response(request))
@@ -47,7 +48,23 @@ trait HTTP {
   }
 
   // Read the given request and return appropriate response.
-  def response(request: ByteString): ByteString = ???
+  def response(request: ByteString): ByteString = {
+    val segments:Array[String] = request.utf8String.split(" ")
+    val method = segments(0)
+    val uri = segments(1)
+    
+    val file = new File(docroot + "/demo/" + uri)
+    method match {
+      case "GET" => {
+    	  if (file.exists && file.isFile()) {
+    	    return ok(file)
+    	  }
+    	  return notFound
+      }
+      case _ => return methodNotAllowed
+    }
+    badRequest
+  } 
 
 }
 
